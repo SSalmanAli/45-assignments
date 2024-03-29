500b74919b18080498bee79ac0fd2d3d8ab6ffdb
SSalmanAli <159621448+SSalmanAli@users.noreply.github.com>
2024-03-29 22:00:52 +0500

    Add files via upload

---
+++ 16.More_Guests/16.ts
@@ -0,0 +1,16 @@
+let people = ["Tommy Shelby" , "Robert Oppenheimer" , "Satoshi Nakamoto" , "Newton"];
+
+let s = ("dear");
+
+let w = ("I would like to invite you to dinner");
+
+let e =(", plz be my honorable guest")
+
+people.forEach(y => console.log(s , y , w , e)); 
+
+let t = ("Imran khan");
+console.log (t); 
+
+console.log ("Everyone i have more space available ")
+
+
